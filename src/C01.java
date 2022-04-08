0b951bbe986b150ecdddc85c7c2cfd74b8849c9c
SelcukTemel <selcuktemell2@gmail.com>
2022-04-08 16:25:08 +0300

    selcuk branch

--- src/C01.java
+++ src/C01.java
@@ -3,5 +3,6 @@ import org.w3c.dom.ls.LSOutput;
 public class C01 {
     public static void main(String[] args) {
         System.out.println("Localden Merhaba");
+        System.out.println("selcuk branch olustu");
     }
 }
