12f62d341799f4260d6748ed93c5b6f6a904113c
SelcukTemel <selcuktemell2@gmail.com>
2022-04-08 16:33:55 +0300

    08.04.2022

--- src/C01.java
+++ src/C01.java
@@ -4,5 +4,6 @@ public class C01 {
     public static void main(String[] args) {
         System.out.println("Localden Merhaba");
         System.out.println("selcuk branch olustu");
+        System.out.println("kadir beye selamlar");
     }
 }
